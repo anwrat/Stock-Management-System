ea28a0d150021723648f6114ca6a01af9d287b11
anwrat <anweshrwt22@gmail.com>
2024-09-30 05:40:16 +0545

    Trying to make a dropdown

--- frontend/src/Interior.tsx
+++ frontend/src/Interior.tsx
@@ -1,5 +1,6 @@
 import { Footerdown } from "./Footer"
 import { Headertop } from "./Header"
+import './dropdown.css'
 
 export function Interior() {
   return(
@@ -7,6 +8,13 @@ export function Interior() {
       <Headertop />
       <div className="middle">
         <h1>This is the Interior Page</h1>
+        <div className="dropdown">
+          <button className="dropbtn">Choose a item</button>
+          <div className="dropdown-content">
+            <p>Royale Bling 20ltr</p>
+            <p>Royale Bling</p>
+          </div>
+        </div>
       </div>
       <Footerdown />
     </>
